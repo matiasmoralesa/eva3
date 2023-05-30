780656197483b958a0454e010a6b1c2799791a3b
electronightx <electronightx@gmail.com>
2023-05-29 22:16:23 -0400

    correccion de campos de form

--- scripts/MyScript.js
+++ scripts/MyScript.js
@@ -1,25 +1,31 @@
 let registra = ()=> {
     let eNombre = document.getElementById("nombre");
-    let eDate = document.getElementById("date");
-    let eTime = document.getElementById("time");
+    let eApellido = document.getElementById("apellido");
     let eCorreo = document.getElementById("correo");
-    let ePassword = document.getElementById("password");
     let eTelefono = document.getElementById("telefono");
+    let eDireccion = document.getElementById("direccion");
+    let eEdad = document.getElementById("edad");
+    let eDate = document.getElementById("date");
+    let eSuscripcion = document.getElementById("suscripcion");
 
     let nombre = eNombre.value;
-    let date = eDate.value;
-    let time = eTime.value;
+    let apellido = eApellido.value;
     let correo = eCorreo.value;
-    let password = ePassword.value;
     let telefono = eTelefono.value;
+    let direccion = eDireccion.value;
+    let edad = parseInt(eEdad.value);
+    let date = eDate.value;
+    let suscripcion = eSuscripcion.value;              
 
     const registro = [];
     registro[0] = nombre;
-    registro[1] = date;
-    registro[2] = time;
-    registro[3] = correo;
-    registro[4] = password;
-    registro[5] = telefono;
+    registro[1] = apellido;
+    registro[2] = correo;
+    registro[3] = telefono;
+    registro[4] = direccion;
+    registro[5] = edad;
+    registro[6] = date;
+    registro[7] = suscripcion;
     console.log(registro);
 }
 
